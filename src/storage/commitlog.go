117654cef6fee2c226c3de8560f010beecdb35e4
Jon Haddad <jon@jonhaddad.com>
2014-03-13 08:55:48 -0700

    starting to think about organization of commit log, pages, files, and db

---
+++ src/storage/commitlog.go
@@ -0,0 +1,7 @@
+package storage
+
+
+/*
+It is recommended that the commit log be stored on a separate disk from the rest of the data files
+this is because, like many dbs, the commitlog is append only.  
+ */
