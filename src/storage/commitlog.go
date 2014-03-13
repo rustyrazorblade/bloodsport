0a2d71402771987e0677b7f9b4c9730677f7aed2
Jon Haddad <jon@jonhaddad.com>
2014-03-13 08:56:15 -0700

    db and file

--- src/storage/commitlog.go
+++ src/storage/commitlog.go
@@ -3,5 +3,5 @@ package storage
 
 /*
 It is recommended that the commit log be stored on a separate disk from the rest of the data files
-this is because, like many dbs, the commitlog is append only.  
+this is because, like many dbs, the commitlog is append only.
  */
