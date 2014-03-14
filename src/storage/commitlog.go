2f6d0774f7c58c2029fd478778074dcbda8a9a8f
Jon Haddad <jon@jonhaddad.com>
2014-03-14 00:23:16 -0700

    top level db

--- src/storage/commitlog.go
+++ src/storage/commitlog.go
@@ -4,6 +4,8 @@ package storage
 /*
 It is recommended that the commit log be stored on a separate disk from the rest of the data files
 this is because, like many dbs, the commitlog is append only.
+
+Data should be writted to the commit log before being saved in memory, as it can be used to replay
  */
 
 type CommitLog struct {
--- src/storage/db.go
+++ src/storage/db.go
@@ -1,6 +1,68 @@
 package storage
 
+import (
+	"rtypes"
+)
+
 /*
 high level database storage.  wraps the commit log and multiple files
 
  */
+
+type DB struct {
+
+}
+
+func (db *DB) Append(key string, value rtypes.VarType) {
+
+}
+
+func (db *DB) Auth(password string) {
+
+}
+
+// noop
+func (db *DB) BGRewriteAOF() {
+}
+
+// noop
+func (db *DB) BGSave() {
+}
+
+func (db *DB) BitCount(key string) {
+
+}
+
+// returns a new string.
+func (db *DB) itOp(operation string) {
+}
+
+func (db *DB) LPop(key string) {
+
+}
+
+// Remove and get the last element in a list, or block until one is available
+func (db *DB) BRPop(key string) {
+
+}
+
+// Pop a value from a list, push it to another list and return it; or block until one is available
+func (db *DB) BRPopLPush() {
+}
+
+
+// delete a key.  seems straightforward.
+func (db *DB) Del(key string) {
+
+}
+
+
+func (db *DB) Incr(key string) {
+
+}
+
+func (db *DB) Set(key string, value rtypes.VarType, ex_seconds int, ex_milliseconds int, nx_or_xx string) {
+
+}
+
+
